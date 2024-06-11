31c89ecb08bf5df5d582097c3e053fc02e197810
Santhosh6207 <165188795+Santhosh6207@users.noreply.github.com>
2024-06-11 21:21:37 +0530

    Time: 1 ms (73.84%), Space: 42 MB (61.60%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int cr[]=new int[1001];
+        ArrayList<Integer>al=new ArrayList<>();
+        
+        int max=-12234556;
+        for(int i=0;i<arr1.length;i++)
+        {
+            if(arr1[i]>max)
+                max=arr1[i];
+        }
+        for(int i=0;i<arr1.length;i++)
+        {
+            cr[arr1[i]]++;
+        }
+        for(int i:arr2)
+        {
+            while(cr[i]>0)
+            {
+                al.add(i);
+                cr[i]--;
+            }
+           
+        }
+        for (int num = 0; num <= max; num++) {
+            while (cr[num] > 0) {
+                al.add(num);
+                cr[num]--;
+            }
+        }
+        for(int j=0;j<al.size();j++)
+        {
+            arr1[j]=al.get(j);
+        }
+        return arr1;
+        
+    }
+}
