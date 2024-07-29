b1aff7246365c395e1cf157e5636edcec6cbb4bf
Santhosh6207 <165188795+Santhosh6207@users.noreply.github.com>
2024-07-30 00:11:03 +0530

    Time: 16 ms (73.64%), Space: 43.9 MB (93.05%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
@@ -2,23 +2,15 @@ import java.util.TreeSet;
 
 class Solution {
     public long subArrayRanges(int[] nums) {
-        long sum = 0;
-        int n = nums.length;
-
-        // Iterate through each starting index of the subarray
-        for (int i = 0; i < n; i++) {
-            TreeSet<Integer> ts = new TreeSet<>();
-            // Iterate through each ending index of the subarray
-            for (int j = i; j < n; j++) {
-                ts.add(nums[j]);
-                // The maximum and minimum in the current subarray
-                int minVal = ts.first();
-                int maxVal = ts.last();
-                // Add the range to the sum
-                sum += maxVal - minVal;
+        long res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int max = nums[i], min = nums[i];
+            for (int j = i; j < nums.length; j++) {
+                max = Math.max(max, nums[j]);
+                min = Math.min(min, nums[j]);
+                res += max - min;
             }
         }
-
-        return sum;
+        return res;
     }
 }
