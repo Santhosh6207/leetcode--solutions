87a9b0043243a88925df6713801ef30722fc77c5
Santhosh6207 <165188795+Santhosh6207@users.noreply.github.com>
2024-08-21 20:54:31 +0530

    Time: 1 ms (24.09%), Space: 42.3 MB (7.62%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,21 +1,19 @@
 class Solution {
     public void sortColors(int[] nums) {
-      int cr[]=new int[3];
-      for(int i=0;i<nums.length;i++)
-      {
-        cr[nums[i]]++;
-      } 
-      int temp=0;
-      for(int i=0;i<3;i++)
-      {  
         
-        int j=0;
-        while(j<cr[i])
-        {
-            nums[temp++]=i;
-            j++;
+        //using priority queue and its properties..
+        // Time Complexity of element addition in priority queue is : O(logn).
+        
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i=0 ; i<nums.length ; i++){
+            pq.add(nums[i]);
         }
-       
-      }
+        
+        for(int i=0 ; i<nums.length ; i++){
+            nums[i] = pq.poll();
+        }
+        
+        // Time Complexity : O(n*logn)
     }
 }
