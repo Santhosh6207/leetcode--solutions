ba6870a5553c5a4c5876bc6e91549aef64006286
Santhosh6207 <165188795+Santhosh6207@users.noreply.github.com>
2024-10-01 15:20:09 +0530

    Time: 2 ms (35.44%), Space: 43.7 MB (22.35%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    HashSet<ArrayList<Integer>>hs=new HashSet<>();
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>>al=new ArrayList<>();
+       Arrays.sort(nums);
+       find(0,nums,al,new ArrayList<>());
+       al.addAll(hs);
+       return al; 
+    }
+    public void find(int idx,int nums[],List<List<Integer>>al,List<Integer>ds)
+    {
+        if(idx==nums.length)
+        {
+          hs.add(new ArrayList<>(ds));
+          return;
+        }
+       
+         ds.add(nums[idx]);
+         find(idx+1,nums,al,ds);
+         ds.remove(ds.size()-1);
+          find(idx+1,nums,al,ds);
+
+    }
+}
