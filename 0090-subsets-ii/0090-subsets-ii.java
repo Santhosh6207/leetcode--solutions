75f1addaaf076d8751da8ac177805044d5e92996
Santhosh6207 <165188795+Santhosh6207@users.noreply.github.com>
2024-10-01 15:24:20 +0530

    Time: 2 ms (35.44%), Space: 43.7 MB (14.75%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,24 +1,26 @@
-class Solution {
-    HashSet<ArrayList<Integer>>hs=new HashSet<>();
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>>al=new ArrayList<>();
-       Arrays.sort(nums);
-       find(0,nums,al,new ArrayList<>());
-       al.addAll(hs);
-       return al; 
-    }
-    public void find(int idx,int nums[],List<List<Integer>>al,List<Integer>ds)
-    {
-        if(idx==nums.length)
-        {
-          hs.add(new ArrayList<>(ds));
-          return;
-        }
-         find(idx+1,nums,al,ds);
-         ds.add(nums[idx]);
-         find(idx+1,nums,al,ds);
-         ds.remove(ds.size()-1);
-          
+import java.util.*;
 
+class Solution {
+    HashSet<List<Integer>> hs = new HashSet<>(); // HashSet to track unique subsets
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> al = new ArrayList<>();
+        Arrays.sort(nums); // Sort the array to handle duplicates easily
+        find(0, nums, al, new ArrayList<>());
+        return al;
+    }
+
+    public void find(int idx, int nums[], List<List<Integer>> al, List<Integer> ds) {
+        // Add the current subset if it is unique (HashSet ensures uniqueness)
+        if (hs.add(new ArrayList<>(ds))) {
+            al.add(new ArrayList<>(ds));
+        }
+
+        // Iterate over remaining elements and recursively build subsets
+        for (int i = idx; i < nums.length; i++) {
+            ds.add(nums[i]); // Include current element
+            find(i + 1, nums, al, ds); // Recurse to explore further subsets
+            ds.remove(ds.size() - 1); // Backtrack by removing the current element
+        }
     }
 }
