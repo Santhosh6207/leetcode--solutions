dcc1788f0825b5df5fe41a3010f8801a0cb76d22
Santhosh6207 <165188795+Santhosh6207@users.noreply.github.com>
2025-02-26 20:50:47 +0530

    Time: 28 ms (89.4%), Space: 55.4 MB (64.79%) - LeetHub

--- 0937-online-stock-span/0937-online-stock-span.java
+++ 0937-online-stock-span/0937-online-stock-span.java
@@ -1,18 +1,30 @@
 class StockSpanner {
-    Stack<int[]>st;
-
+      Stack<pair>st;
     public StockSpanner() {
-       st=new Stack<>(); 
+        st=new Stack<>();
     }
     
     public int next(int price) {
-        int ans=1;
-        while(!st.isEmpty()&&st.peek()[0]<=price)
-          ans+=st.pop()[1];
-          st.push(new int[]{price,ans});
-        return ans;
+       int ans=1;
+       while(!st.isEmpty()&&st.peek().price<=price)
+       {
+          ans+=st.pop().span;
+       }
+          st.push(new pair(price,ans));
+       return ans;
     }
 }
+class pair
+{
+    int price;
+    int span;
+    public pair(int price,int span)
+    {
+        this.price=price;
+        this.span=span;
+    }
+}
+
 
 /**
  * Your StockSpanner object will be instantiated and called as such:
