4664314336f5329368eb57f44f04d76f969d7a51
Santhosh6207 <165188795+Santhosh6207@users.noreply.github.com>
2025-11-07 21:08:06 +0530

    Time: 563 ms (13.61%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,6 +1,2 @@
-# Write your MySQL query statement below
-#select w1.id from weather w1 self join weather w2 on w1.temperature>w2.temperature;
-SELECT w1.id
-FROM weather w1
-JOIN weather w2 ON w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY)
-WHERE w1.temperature > w2.temperature;
+select distinct w.id from weather w join weather s on w.id!=s.id and
+ w.temperature>s.temperature and DATEDIFF( w.recorddate, s.recorddate)=1 
